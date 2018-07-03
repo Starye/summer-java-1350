99b047b5544a844463439ef45b84f103405671e2
414 <414@DESKTOP-KQTPDG3>
2018-07-03 16:59:40 +0900

    1

---
+++ src/my/day02/nselect.java
@@ -0,0 +1,15 @@
+package my.day02;
+
+import java.util.*;
+
+public class nselect {
+
+	public static void main(String[] args) {
+		int num;
+		Random rand = new Random();
+		num = rand.nextInt(101);
+		System.out.println("컴퓨터가 생성한 난수 : "+num);
+		
+	}
+
+}
